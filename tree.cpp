317615f00c6b7c61fd198faa6e32896f9732ff8f
hackerX400 <gangwarsanu8@gmail.com>
2024-07-26 17:37:29 +0530

    Tree from scratch

---
+++ tree.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <bits/stdtr1c++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+node *buildTree(node *root)
+{
+    cout << "enter the data:" << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+    if (data == -1)
+    {
+        return NULL;
+    }
+    cout << "enter data for inserting in root left " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "enter data for inserting in root right " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+void levelOrderTraversal(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+// LNR
+void inOrder(node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+// NLR
+void preOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+// LRN
+void postOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+int main()
+{
+    node *root = NULL;
+    //  creating a tree
+    root = buildTree(root);
+    // 1 5 10 -1 -1 12 -1 -1 6 13 -1 -1 14 -1 -1
+    // traversing the root
+    cout << "printing travel order " << endl;
+    levelOrderTraversal(root);
+    cout << "printing inoder traversel" << endl;
+    inOrder(root);
+    cout<<endl;
+    cout << "printing preoder traversal" << endl;
+    preOrder(root);
+    cout<<endl;
+    cout << "printing post order travesal" << endl;
+    postOrder(root);
+    return 0;
+}
